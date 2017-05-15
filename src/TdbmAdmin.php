5e863dbe67e74e9d2d05f68d6909bc7e3f4a77f0
David NÃ©grier <d.negrier@thecodingmachine.com>
2017-05-15 16:07:21 +0200

    Altering composer.json to use new thecodingmachine/tdbm package.

--- src/TdbmAdmin.php
+++ src/TdbmAdmin.php
@@ -13,6 +13,6 @@ $moufManager->declareComponent('tdbmadmin', 'Mouf\\Database\\TDBM\\Controllers\\
 $moufManager->bindComponents('tdbmadmin', 'template', 'moufTemplate');
 $moufManager->bindComponents('tdbmadmin', 'content', 'block.content');
 
-$moufManager->declareComponent('tdbminstall2', 'Mouf\\Database\\TDBM\\Controllers\\TdbmInstallController', true);
-$moufManager->bindComponents('tdbminstall2', 'template', 'moufInstallTemplate');
-$moufManager->bindComponents('tdbminstall2', 'content', 'block.content');
+$moufManager->declareComponent('tdbminstall3', 'Mouf\\Database\\TDBM\\Controllers\\TdbmInstallController', true);
+$moufManager->bindComponents('tdbminstall3', 'template', 'moufInstallTemplate');
+$moufManager->bindComponents('tdbminstall3', 'content', 'block.content');
