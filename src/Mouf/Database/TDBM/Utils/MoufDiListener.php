eb6d8c8a7043415bd70a1fa12f540fa42370afc1
David Négrier <d.negrier@thecodingmachine.com>
2019-07-29 15:28:00 +0200

    Adopting code to the new DaoFactory that lazy load DAOs from the container

--- src/Mouf/Database/TDBM/Utils/MoufDiListener.php
+++ src/Mouf/Database/TDBM/Utils/MoufDiListener.php
@@ -9,6 +9,7 @@ use TheCodingMachine\TDBM\TDBMService;
 use Mouf\MoufManager;
 use TheCodingMachine\TDBM\Utils\GeneratorListenerInterface;
 use TheCodingMachine\TDBM\Utils\TDBMDaoGenerator;
+use function var_export;
 
 class MoufDiListener implements GeneratorListenerInterface
 {
@@ -34,14 +35,29 @@ class MoufDiListener implements GeneratorListenerInterface
         foreach ($beanDescriptors as $beanDescriptor) {
             $daoName = $beanDescriptor->getDaoClassName();
 
+            // Rename all DAOs to full-class name (migration from 5.0 to 5.1+ naming schema)
             $instanceName = TDBMDaoGenerator::toVariableName($daoName);
+            if ($moufManager->instanceExists($instanceName) && !$moufManager->instanceExists($configuration->getDaoNamespace().'\\'.$daoName)) {
+                $moufManager->renameComponent($instanceName, $configuration->getDaoNamespace().'\\'.$daoName);
+
+                // Let's create a "Link" between old Mouf::getXXXDao and new Mouf::getNamespaceXXXDao
+                $moufManager->createInstanceByCode()->setName($instanceName)->setCode('return $container->get('.var_export($configuration->getDaoNamespace().'\\'.$daoName, true).');');
+
+                // Let's unset the setter. It is useless now.
+                if ($daoFactoryInstanceName !== null) {
+                    $moufManager->unsetParameterForSetter($daoFactoryInstanceName, 'set'.$daoName);
+                }
+            }
+
+            $instanceName = $configuration->getDaoNamespace().'\\'.$daoName;
             if (!$moufManager->instanceExists($instanceName)) {
                 $moufManager->declareComponent($instanceName, $configuration->getDaoNamespace().'\\'.$daoName);
             }
             $moufManager->setParameterViaConstructor($instanceName, 0, $tdbmServiceInstanceName, 'object');
-            if ($daoFactoryInstanceName !== null) {
-                $moufManager->bindComponentViaSetter($daoFactoryInstanceName, 'set'.$daoName, $instanceName);
-            }
+        }
+
+        if ($daoFactoryInstanceName !== null) {
+            $moufManager->setParameterViaConstructor($daoFactoryInstanceName, 0, 'return $container;', 'primitive', 'php');
         }
 
         $moufManager->rewriteMouf();
